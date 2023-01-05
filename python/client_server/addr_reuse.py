f5a95a4466e7b59aa5a820be27233850fe1f982a
Lucas Zunho <lucaszunho17@gmail.com>
2023-01-04 21:15:39 -0300

    Completado script de reutilização de endereço (SO_REUSEADDR)

--- python/client_server/addr_reuse.py
+++ python/client_server/addr_reuse.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 # addr_reuse.py -- Address reuse example
 # Lucas Zunho <lucas.zunho@zunho.com.br> (c) MIT
+#
+# With SO_REUSEADDR you can close the server and rerun it on the same port --
+# this program is a simple use example
 
 import socket
 import sys
 import argparse
 
 HOST = "127.0.0.1"
-PORT = 0
+PORT = 53455
 
 def main():
-    # Commandline arguments
-    argp 
     
     # Creating file descriptor
     try:
         sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as e:
-        sys.exit("Err. => File descriptor returned an exception: %s".format(e))
+        sys.exit("Err. => File descriptor returned an exception: {}".format(e))
 
     # Getting old address socket state
     sock_config = sockfd.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
@@ -27,6 +28,26 @@ def main():
     sockfd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock_config = sockfd.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
     print("New State => {}".format(sock_config))
+
+    # Creating new file descriptor
+    try:
+        newfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        sys.exit("Err. => File descriptor returned an exception: {}".format(e))
+
+    # Reuse config
+    newfd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    newfd.bind((HOST, PORT))
+    newfd.listen(1)
+    print("[*] Listening on port {}".format(PORT))
+    while True:
+        try:
+            clientfd, clientaddr = newfd.accept()
+            print("\t=> Connection From {}:{}".format(clientaddr[0], clientaddr[1]))
+        except KeyboardInterrupt:
+            break
+        except socket.error as e:
+            sys.exit("Err. => Connection loop returned an error: {}".format(e))
 
 if __name__ == "__main__":
     main()
