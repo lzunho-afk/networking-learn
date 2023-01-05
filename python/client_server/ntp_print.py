2bead8f9069e47c2ad21d3e7fc250c61090c61e6
Lucas Zunho <lucaszunho17@gmail.com>
2023-01-05 20:38:59 -0300

    Criação de subparsers para os argumento de linha de comando

--- python/client_server/ntp_print.py
+++ python/client_server/ntp_print.py
@@ -4,6 +4,7 @@
 
 from time import ctime
 import ntplib
+import sys
 import argparse
 
 def ntplib_time(host):
@@ -17,18 +18,23 @@ def skeleton_time(host, port):
 def main():
     # Command-line arguments
     argp = argparse.ArgumentParser(prog="ntp_print.py", description="NTP Print program")
-    argp.add_argument('-c', '--connection-mode', choices=['NTPLIB', 'SKELETON'], action="store", dest="mode", default='NTPLIB', required=True)
-    argp.add_argument('--hostname', action="store", dest="host", default='pool.ntp.org', required=False)
-    argp.add_argument('-p', '--port', type=int, choices=range(1,65535), metavar='PORT', action="store", dest="port", default=0, required=False)
-    args = argp.parse_args()
-    connection_mode = args.mode
-    host = args.host
-    port = args.port
+    sargp = argp.add_subparsers(dest="connection_mode", help="Connection modes help", required=True)
 
-    if connection_mode == 'NTPLIB':
-        ntplib_time(host)
-    else:
-        skeleton_time(host, port)
+    # NTPLIB Parser
+    ntplib_argp = sargp.add_parser("ntplib", help="Uses ntplib connection module")
+    ntplib_argp.add_argument('--hostname', action="store", dest="host", help='NTP Server to connect', default='pool.ntp.org', required=False)
+
+    # SKELETON Parser
+    skeleton_argp = sargp.add_parser("skeleton", help="Uses pure socket communication")
+    skeleton_argp.add_argument('--hostname', action="store", dest="host", help='NTP Server to connect', required=True)
+    skeleton_argp.add_argument('-p', '--port', type=int, choices=range(1,65535), metavar='PORT', action="store", dest="port", help='NTP Server port', default=123, required=False)
+
+    args = argp.parse_args()
+
+    if args.connection_mode == 'ntplib':
+        ntplib_time(args.host)
+    elif args.connection_mode == 'skeleton':
+        skeleton_time(args.host, args.port)
 
 if __name__ == "__main__":
     main()
